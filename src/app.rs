55e4066a4337a28a0fcb5ff29157858915a5986f
Kyle Green <kyle.green999@gmail.com>
2023-05-09 15:18:49 +0100

    Added menu component

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,19 @@
 use yew::prelude::*;
 
+use crate::components::menu::Menu;
+
 #[function_component(App)]
 pub fn app() -> Html {
-    html! {
+    let is_menu_open = use_state_eq(|| false);
 
+    html! {
+        <div class="flex-row stretch no-scroll">
+            <Menu is_open={is_menu_open}>
+                <h2>{"Random table tool"}</h2>
+            </Menu>
+            <main class="flex-grow-1 stretch-height">
+                
+            </main>
+        </div>
     }
 }
